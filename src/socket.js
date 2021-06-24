9b0ed5be5da7c456000e139c86a67f53291c5531
visily <michael4161@hotmail.com>
2021-06-24 01:18:02 -0600

    sdf

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,3 @@
 import io from 'socket.io-client';
-const socketURL =  process.env.PORT  || 'http://localhost:5000'; 
+const socketURL =  "https://sprouts-control-center.herokuapp.com/"  || 'http://localhost:5000'; 
 export const socket = io(socketURL);
