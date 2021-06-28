2b8b7f7d6fb3b2aadef5f01589469483dff42f4b
visily <michael4161@hotmail.com>
2021-06-28 01:54:05 -0600

    fixed but with time zone difference and created files for refactor button and modal

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,20 +5,25 @@ import {socket} from '../socket';
 import './home.css';
 
 function Home() {
-    // use to set initial state for days of week
+    //TODO:
     const [weekDates, setWeekDates] = useState([]);
+    const [jobs, setJobs] = useState();
+    const [modalDisplay, setModalDisplay] = useState(false);
+    
+    
+    
+    // use to set initial state for days of week
     useEffect(() => {
         let cur = moment();
         let firstday = cur.clone().weekday(0);
         let lastday = cur.clone().weekday(6);
-
+        
         let dates = [firstday,lastday];
         setWeekDates(dates);
     }, [])
-
-
-    // set job data
-    const [jobs, setJobs] = useState();
+    
+    
+    
     useEffect(() => {
         if (weekDates.length !== 0){
             socket.emit('pgInit', weekDates);
@@ -26,77 +31,31 @@ function Home() {
         if (weekDates.length !== 0){
             socket.once('initJobs', (data) => {
                 let InitJson = [
-                {day:"", jobs:[]},
-                {day:"", jobs:[]},
-                {day:"", jobs:[]},
-                {day:"", jobs:[]},
-                {day:"", jobs:[]},
-                {day:"", jobs:[]},
-                {day:"", jobs:[]}];
-                
-                // creates the day of the week for the array of json
-                for(let i=0; i < InitJson.length; i++){      
-                    let iterDate = weekDates[0].clone().add(i,'days');
-                    console.log(iterDate)
-                    InitJson[i].day = iterDate;
-                }
+                    {day:"", jobs:[]},
+                    {day:"", jobs:[]},
+                    {day:"", jobs:[]},
+                    {day:"", jobs:[]},
+                    {day:"", jobs:[]},
+                    {day:"", jobs:[]},
+                    {day:"", jobs:[]}];
+                    
+                    // creates the day of the week for the array of json
+                    for(let i=0; i < InitJson.length; i++){      
+                        let iterDate = weekDates[0].clone().add(i,'days');
+                        InitJson[i].day = iterDate;
+                    }
                     // creates the job info from socket
-                for(let i=0; i < data.length;i++){
-                    let iter = moment(data[i].book_date)
-                    InitJson[iter.day()].jobs.push(data[i]);
-                }
-                // set the information for the useState
-                setJobs(InitJson);
-                console.log(InitJson);
+                    for(let i=0; i < data.length;i++){
+                        let iter = moment(new Date(data[i].book_date).toISOString().split('T')[0])
+                        InitJson[iter.day()].jobs.push(data[i]);
+                    }
+                    // set the information for the useState
+                    setJobs(InitJson);
                 });
-        }
+            }
             
-    },[weekDates]);
-
-
-    const ShowWeeks = (props) => {
-        // map the 7 days of the week from jobs
-        const dayofweek = props.jobs.map((day,index) => {
-        console.log(day.day.format('dddd'))
-            return( 
-        <div className='altrItem'>
-            <div className='box' key={day.day}>
-                <h2 className='weekDate'>
-                    { day.day.format('MMMM') + " " +day.day.format('dddd') +" "+ day.day.date()}
-                </h2>
-                <AiIcons.AiOutlineUserAdd id={index} className='AddIcon' onClick={modalToggle}/>
-            </div>
-            <div className='numJobs'>Job Count: {day.jobs.length}</div>
-        </div>
-           )
-        });
-
-        return(
-        <div className='topLevelJob'>
-            {dayofweek}
-        </div>
-        )
-    }
-
-    //  allows you to see and edit data in old job
-    const selectedJob = (props) => {
-
-    }
-
-    //TODO:
-    // consists of the modal that creates new job
-    const [modalDisplay, setModalDisplay] = useState(false);
-
-    const modalToggle = () => setModalDisplay(!modalDisplay);
-    
-    const NewJob = (props) => {
-        // console.log(props.data)
-        return(
-        <></>
-            );
-    }
-
-
+        },[weekDates]);
+        
     // sets the weekDate to previous week
     const prevWeek = () => {
         const first = weekDates[0].clone().add(-7,'days');
@@ -110,7 +69,30 @@ function Home() {
         const last = weekDates[1].clone().add(7,'days');
         setWeekDates([first ,last] )
     }
+        
+        
+    const ShowWeeks = (props) => {
+        // map the 7 days of the week from jobs
+        const dayofweek = props.jobs.map((day,index) => {
+            return( 
+                <div className='altrItem'key={day.day}>
+                    <div className='box' >
+                        <h2 className='weekDate'>
+                            { day.day.format('MMMM') + " " +day.day.format('dddd') +" "+ day.day.date()}
+                        </h2>
+                        <AiIcons.AiOutlineUserAdd id={index} className='AddIcon' onClick={modalToggle}/>
+                    </div>
+                    <div className='numJobs'>Job Count: {day.jobs.length}</div>
+                </div>
+            )
+        });
 
+    return(
+        <div className='topLevelJob'>
+            {dayofweek}
+        </div>
+        )
+    }
 
     if(weekDates[0] && jobs !== undefined){
         return (
@@ -120,16 +102,12 @@ function Home() {
                     <button className='transButton' onClick={nextWeek}>Next</button>
                 </div>
                 <ShowWeeks jobs={jobs}/>
-                <NewJob/>
             </div>
         )        
     }
-    console.log(weekDates + jobs)
-    return (
-        <div>
-            {/* loading */}
-        </div>
-    )
+    else {
+        return (<></>)
+    }
 }
 
 export default Home
